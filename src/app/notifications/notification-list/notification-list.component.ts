0457b4d39d720d63ff2f35eacae3b0a2e60c7068
nikki9119 <a.p.nikhilkumar@gmail.com>
2020-08-07 19:59:34 +0530

    added completed notifications module

--- src/app/notifications/notification-list/notification-list.component.ts
+++ src/app/notifications/notification-list/notification-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NotificationsService, Command } from '../notifications.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-notification-list',
@@ -6,10 +8,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./notification-list.component.css']
 })
 export class NotificationListComponent implements OnInit {
+  messages: Observable<Command[]>;
 
-  constructor() { }
+  constructor(private notificationsService: NotificationsService) {
+    this.messages = this.notificationsService.messagesOutput;
+   }
 
   ngOnInit(): void {
   }
 
+  clearMessage(id: number){
+    this.notificationsService.clearMessage(id);
+  }
 }
--- src/app/notifications/notifications.service.ts
+++ src/app/notifications/notifications.service.ts
@@ -1,9 +1,65 @@
 import { Injectable } from '@angular/core';
+import { Subject, Observable } from 'rxjs';
+import { scan } from 'rxjs/operators';
+
+export interface Command{
+  id: number;
+  type: 'sucess' | 'error' | 'clear';
+  text?: string;
+};
 
 @Injectable({
   providedIn: 'root'
 })
 export class NotificationsService {
+  messagesInput: Subject<Command>;
+  messagesOutput: Observable<Command[]>;
 
-  constructor() { }
+  constructor() { 
+    this.messagesInput = new Subject<Command>();
+    this.messagesOutput = this.messagesInput.pipe(
+      scan((acc: Command[], value) => {
+        if(value.type === 'clear'){
+          return acc.filter(message => message.id !== value.id);
+        } else {
+          return [...acc,value];
+        }
+      }, [])
+    );
+  }
+
+  addSuccess(message: string){
+    const id = this.randomId()
+    this.messagesInput.next({
+      id,
+      text: message,
+      type: 'sucess'
+    });
+    setTimeout(() => {
+      this.clearMessage(id);
+    },5000);
+  }
+
+  addError(message: string){
+    const id = this.randomId();
+    this.messagesInput.next({
+      id,
+      text: message,
+      type: 'error'
+    });
+    setTimeout(() => {
+      this.clearMessage(id);
+    },5000);
+  }
+
+  clearMessage(id: number){
+    this.messagesInput.next({
+      id: id,
+      type: 'clear'
+    });
+  }
+
+  private randomId(){
+    return Math.round(Math.random()*10000);
+  }
 }
